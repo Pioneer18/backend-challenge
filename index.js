aaddf95de0b9b7150a43764e93bf539eb62797a7
sellsj14 <sellsj14@gmail.com>
2018-12-28 17:03:26 -0500

    added body parser middleware. still working out the structure of the app before continuting

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 //Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 //initialize express
 const app = express;
+
+//parse incoming HTTP request bodies to capture submitted physician data
+app.use(bodyParser.json());
+//allow any data types inside of the parsed request object
+app.use(bodyParser.urlencoded({ extended: true }) );
 
 //setup mongoose (enable es6 promises and specify the database name/location)
 mongoose.Promise = Promise;
