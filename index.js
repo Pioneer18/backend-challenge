19fc9eac557ea825177d428cb8b7d948c3abc634
sellsj14 <sellsj14@gmail.com>
2018-12-28 16:13:32 -0500

    utilizing promises with mongoose, specified connection to mongodb collection 'physicians'. will add event handler for indicating db successfully connected to

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const mongoose = require('mongoose');
 //initialize express
 const app = express;
 
+//setup mongoose (enable es6 promises and specify the database name/location)
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost:27017/physicians');
+
 //serve the static public directory 
 app.use(express.static('public'));
