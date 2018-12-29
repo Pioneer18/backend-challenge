d0a846e5ad399e292bfc19c466bfbeca29daffd2
sellsj14 <sellsj14@gmail.com>
2018-12-29 16:40:18 -0500

    added a contractStatus key with nested keys such as facitlity. data-structure is still open to change at this point, just filling it in

--- src/models/physicians/schema.js
+++ src/models/physicians/schema.js
@@ -10,12 +10,30 @@ const physician = new Schema({
         type: String,
         required: true,
     },
-    specialty:{
+    specialty: {
         type: [String], //Some doctors may have more than one specialty
         default: undefined
     },
     contractStatus: {
-        
+        employeed: {
+            type: Boolean,
+            required: true //if false the following should be undefined 
+        },
+        hospitalGroup: {
+            type: String,
+            default: "unkown",
+        },
+        facitlity: {
+            name: { type: String, required: true },
+            address: {
+                street: { type: String, required: true },
+                suiteNum: { type: Number, default: null },
+                buildingNum: { type: Number, default: null },
+                city:{ type: String, required: true },
+                state:{ type: String, required: true },
+                zip: { type: Number, required: true}
+            }
+        }
+
     }
-    
 })
