40cfa9c8fce63ce47b0f5adf6f28bb9d338b0687
sellsj14 <sellsj14@gmail.com>
2018-12-31 02:26:26 -0500

    corrected comments in physicians schema and changed name to physicians

--- src/models/physicians/schema.js
+++ src/models/physicians/schema.js
@@ -1,14 +1,16 @@
 /**
  * Schema Design
  * embedded facility and county subdocument arrays containing hospital and counties, hospitals contain correlated county id's
- * Assumption: 
- *  1) no need for separate county or faciltiy(location) doucments, physicans contain them
- *  2) Ok to delete Physicains and facility /county subdocuments. 
+ * Assumptions: 
+ *  1) no need for separate county or faciltiy(location) documents, physicans contain them as sub documents
+ *  2) Ok to delete Physicains and facility /county subdocuments; all counties & hospitals related to our collection of physicians will be represented
+ *  3) facilities or counties not found in a query indicate our collection currently does not have a physician representing that location
+ *  4) by embedding the counties in the facility subdocument, the effect of counties 'having multiple hospitals and hospitals having multiple physicians' is achieved
  */
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Physician = new Schema({
+const physcians = new Schema({
     firstName: {
         type: String,
         required: true
@@ -31,4 +33,4 @@ const Physician = new Schema({
     } ] 
 })
 
-module.exports = { Physicain };
+module.exports = { physcians };
