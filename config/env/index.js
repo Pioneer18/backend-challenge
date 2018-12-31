1e2f7358762bf680416e91806738c94391644d33
sellsj14 <sellsj14@gmail.com>
2018-12-31 12:22:47 -0500

    setting up src/api/index.js to act as a router to be exported to app.js entry point. preparing to test first route with this api structure

--- config/env/index.js
+++ config/env/index.js
@@ -1,4 +1,4 @@
-//determine the node environment for the db to connect
+//determine the node environment for the db to connect (There is only devlopment for this app)
 const env = process.env.N0DE_ENV || 'development';
 //this will be the entry point
 const config = require(`./${env}`);
--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@
 //and pass the modles and necessary config to the controllers to handle the 
 //request at the endpoints
 
-const express = require('express');
+const config = require('../config');
+
+//import middleware here to attach to router
+
+//import the models
+const models = require('./models');
+
+//import physician controller to register to endpoint
+const physicians = require('./controllers/physicians');
+
+const routersInit = config => {
+    const router = express();
+
+    //register api points (endpoints with controllers)
+    //at this reative endpoint, call the physicians controller with access to models and config
+    router.use('/physicians', physicians(models, { config }));
+
+    //register middleware here
+
+    return router;
+};
+
+module.exports = routersInit;
