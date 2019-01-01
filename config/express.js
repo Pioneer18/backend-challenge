59389b78c1643ad8a43cfdd362e3ee645269b032
sellsj14 <sellsj14@gmail.com>
2018-12-31 21:43:50 -0500

    added the express-session persitence to the express configuration

--- config/express.js
+++ config/express.js
@@ -7,5 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 //allow any data types inside of the parsed request object
 app.use(bodyParser.urlencoded({ extended: true }) );
+//session handling for passport
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 module.exports = app;
