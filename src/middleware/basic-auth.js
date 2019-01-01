f18855c09cd4c03d299bd16917fc795e591a10ef
sellsj14 <sellsj14@gmail.com>
2019-01-01 14:06:48 -0500

    returned challenge option to true, the browser will store user and password and auto use on authenticated routes till browser restarted. no need to add a db for users, this basic auth setup is working

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -7,7 +7,7 @@ const Authentication = basicAuth({
     'admin': 'supersecret', //admin can use all CRUD ops
     'guest': 'justlookin'   //guest can read only
   },
-  challenge: false,
+  challenge: true, //credentials are cached and auto used till browser restarts
   unauthorizedResponse: getUnauthorizedResponse
   //NOTE: if a request is not authorized a message explaining why (and a 401 error) will be returned
 })
