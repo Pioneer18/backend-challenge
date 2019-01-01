206afe1e5fe727c4eb6851db97fb890f13014d16
sellsj14 <sellsj14@gmail.com>
2019-01-01 14:51:30 -0500

    slight problem with config variable, moving mongodb setup off the app.js entry point

--- app.js
+++ app.js
@@ -2,25 +2,11 @@
 //config
 const app = require('./config/express');
 const config = require('./config/env/');
-const mongoose = require('mongoose');
 const api = require('./src/api/index');
+const mongoManager = require('./src/mongo/mongoose');
 
-
-//setup mongoose (enable es6 promises and specify the database name/location)
-mongoose.Promise = Promise;
-mongoose.connect(config.db, { useNewUrlParser: true });
-
-//mongoose connection event handlers
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.db}`);
-});
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to database: ${config.db}`);
-});
-
-if (config.env === 'development') {
-  mongoose.set('debug', true); //enable logging collection methods + arguments to the console
-}
+//initialize mongoManager
+mongoManager(config);
 
 //api routes v1; register the src/api/index.js router and its relative routes (/physician, /test) 
 //serve the static public directory 
---
+++ src/mongo/mongoose.js
@@ -0,0 +1,22 @@
+
+const mongooseInit = () => {
+    const mongoose = require('mongoose');
+
+    //setup mongoose (enable es6 promises and specify the database name/location)
+    mongoose.Promise = Promise;
+    mongoose.connect(config.db, { useNewUrlParser: true });
+
+    //mongoose connection event handlers
+    mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${config.db}`);
+    });
+    mongoose.connection.on('connected', () => {
+    console.log(`Connected to database: ${config.db}`);
+    });
+
+    if (config.env === 'development') {
+    mongoose.set('debug', true); //enable logging collection methods + arguments to the console
+    }
+}
+
+module.exports = mongooseInit;
