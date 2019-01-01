ab685c26dd17d1d8a992a92f14c09dea9b02c8a6
sellsj14 <sellsj14@gmail.com>
2019-01-01 11:41:16 -0500

    adding basicAuth middleware setup onto app.js main entry point

--- app.js
+++ app.js
@@ -5,27 +5,33 @@ const config = require('./config/env/');
 const mongoose = require('mongoose');
 const api = require('./src/api/index');
 
+//temporairly setting up express-basic-auth on the main entry point
+const basicAuth = require('express-basic-auth');
+app.use(basicAuth({
+  users: { 'admin': 'supersecret' }
+}))
+
 //setup mongoose (enable es6 promises and specify the database name/location)
 mongoose.Promise = Promise;
-mongoose.connect(config.db,{useNewUrlParser: true});
+mongoose.connect(config.db, { useNewUrlParser: true });
 
 //mongoose connection event handlers
 mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${config.db}`);
-  });
-  mongoose.connection.on('connected', () => {
-    console.log(`Connected to database: ${config.db}`);
-  });
-  
-  if (config.env === 'development') {
-    mongoose.set('debug', true); //enable logging collection methods + arguments to the console
-  }
+  throw new Error(`unable to connect to database: ${config.db}`);
+});
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to database: ${config.db}`);
+});
+
+if (config.env === 'development') {
+  mongoose.set('debug', true); //enable logging collection methods + arguments to the console
+}
 
 //api routes v1; register the src/api/index.js router and its relative routes (/physician, /test) 
 //serve the static public directory 
 app.use(express.static('public'));
 app.use('/api/v1', api(config));
 
-app.listen(config.port, ()=>{
-    console.log(`api listening on port ${config.port} ${config.env}`);
+app.listen(config.port, () => {
+  console.log(`api listening on port ${config.port} ${config.env}`);
 })
