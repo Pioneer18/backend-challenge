bd464d0ba6bdc07e105722a86aead4484f51b4c6
sellsj14 <sellsj14@gmail.com>
2019-01-01 20:25:17 -0500

    the entire physician document is available to be updated by the request.body data

--- src/controllers/physicians/update.js
+++ src/controllers/physicians/update.js
@@ -2,14 +2,35 @@
 
 const update = ({ Physicians }) => async (req, res, next) => {
     //validate the request
-    if(!req.body) {
+    if (!req.body) {
         return res.status(400).send({
             message: "Please select a physician"
         });
-    }    
-    
-    try {
+    }
 
+    try {
+        //find the physician document and update it with the req.body
+        //use req.params.physicianId to select the correct physician document
+        Physicians.findByIdAndUpdate(req.params, {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            specialty: [{ specialty: req.body.specialty }],
+            contractStatus: {
+                employeed: req.body.contractStatus
+            },
+            term: {
+                years: req.body.termYears,
+                months: req.body.termMonths,
+                start: req.body.termStart,
+                end: req.body.termEnd
+            },
+            state: req.body.state,
+            facility: [{
+                name: req.body.facilityName,
+                address: { street: req.body.facilityStreet, city: req.body.facilityCity, suite: req.body.facilitySuite },
+                county: req.body.facilityCity
+            }]
+        });
 
 
     } catch (error) {
