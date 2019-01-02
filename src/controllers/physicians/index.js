0461e711aeb70cd33a2eced22a155ebadcdd3804
sellsj14 <sellsj14@gmail.com>
2019-01-02 13:30:18 -0500

    added to PUT /api/v1/physicians/:_id documentation [CAUTION] this route requires every path of a physician be updated or it will default to null. havent found how to update one path value only and leave the others as they were. could make other routes to update specific physician paths but that will increase #of routes, leaving as is for now

--- src/controllers/physicians/index.js
+++ src/controllers/physicians/index.js
@@ -23,7 +23,7 @@ GET /api/v1/facilityByCounty
 GET /api/v1/physicians/:_id - get single physician
 
 POST /api/v1/physicians/ - create
-@header //add permission leve1
+@authorization basic-static username:password
 @param 
     firstName (require) - {String}
     lastName (require) - {String}
@@ -32,8 +32,9 @@ POST /api/v1/physicians/ - create
     state (require) - {String}
     facility (require) - [{ name: String , address: {String, String, Number}, county: (require) - {String} }]
 
-PUT /api/v1/physicians/:_id
-@header //add permission level
+
+PUT /api/v1/physicians/:_id - findOneAndUpdate() [CAUTION: user must enter ever path of a physician to be updated - or it will be turned to null] 
+@authorization basic-static username:password
 @param
     firstName - {String}
     lastName - {String}
@@ -54,20 +55,28 @@ module.exports = (models, { config }) => {
 
     //on route api/v1/physicians call the list.js controller to list all physicians
     api.get('/', list(models, { config }));
+
     //on route api/v1/physicians/:_id call the get controller to find physician document by _id 
     api.get('/:_id', get(models, { config }));
+
     //on route api/v1/filtered call the filtered.js controller to list all physician filtered by user input
     api.get('/filtered', filtered(models, { config }));
+
     //on route api/v1/facilityByCounty call the facilityByCounty.js controller to find all facility locations in a selected county
     api.get('/facilityByCounty', facilityByCounty(models, { config }));
+
     //on route api/v1/physicians/get call the get.js controller to retrieve specified physician(s)
     api.get('/:_id', get(models, { config }));
+
     //on route api/v1/physicians/create call the create.js controller to add a new physician to the db
     api.post('/', create(models, { config }));
+
     //on route api/v1/physicians/update call the update.js controller to update the info on a physician(s) in the db
     api.put('/:_id', update(models, { config }));
+
     //on route api/v1/physicians/remove call the remove.js controller to delete a physician from the d
     api.delete('/:_id', remove(models, { config }));
+
 
 
     //this is now a function to return a route using the list.js physicians controller file
