8f504e0be8b3484f53d13300c8fa7179983abd94
sellsj14 <sellsj14@gmail.com>
2019-01-02 02:37:25 -0500

    filter route was actually not working when entire schema entered, slowing piecing it together to see why nothing was being returned

--- src/controllers/physicians/filtered.js
+++ src/controllers/physicians/filtered.js
@@ -2,32 +2,10 @@
 
 const filtered = ({ Physicians }) => async (req, res, next) => {
     try {
-        const filter = await Physicians.find(
-            //find where the firstName and lastName paths match the client input (from a search)
-            {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                specialty: [{ specialty: req.body.specialty }],
-                contractStatus: {
-                    employeed: req.body.contractStatus
-                },
-                term: {
-                    years: req.body.termYears,
-                    months: req.body.termMonths,
-                    start: req.body.termStart,
-                    end: req.body.termEnd
-                },
-                state: req.body.state,
-                facility: [{
-                    name: req.body.facilityName,
-                    address: {
-                        street: req.body.facilityStreet,
-                        city: req.body.facilityCity,
-                        suite: req.body.facilitySuite
-                    },
-                    county: req.body.facilityCounty
-                }]
-            });
+        let filter = await Physicians.find({ 
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        });
         return res.send(filter);
 
     } catch (error) {
--- src/controllers/physicians/list.js
+++ src/controllers/physicians/list.js
@@ -2,7 +2,7 @@
 
 const list = ({ Physicians }) => async (req, res, next) => {
     try {
-        let listed = await Physicians.find();
+        let listed = await Physicians.find({firstName: req.body.firstName});
         return res.send(listed);
 
     } catch (error) {
