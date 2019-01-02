61305d3138511c989195fb487ccf8acd36bf3a10
sellsj14 <sellsj14@gmail.com>
2019-01-02 13:55:35 -0500

    added route to find single physician by id

--- src/controllers/physicians/get.js
+++ src/controllers/physicians/get.js
@@ -1,8 +1,9 @@
-//fetch a specific physician(s) by name
+//fetch a specific physician(s) by _id e.g. user clicks on a listed physician to view the document
 
 const get = ({ Physicians }) => async (req, res, next) => {
     try {
-        res.send("fetching a physician(s) by name");
+        let physician = await Physicians.findOne(req.params.physiciansId);
+        return res.send(physician);
     } catch (error) {
         next(error);
     }
