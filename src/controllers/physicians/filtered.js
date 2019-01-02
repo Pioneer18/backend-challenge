0faa03397e6d0c461d1d0dd27539f851bd6b9b71
sellsj14 <sellsj14@gmail.com>
2019-01-02 10:34:57 -0500

    removed commented out previous query with $match. creating a strictFiltered route and facilityByCounty route next

--- src/controllers/physicians/filtered.js
+++ src/controllers/physicians/filtered.js
@@ -2,46 +2,24 @@
 
 const filtered = ({ Physicians }) => async (req, res, next) => {
     try {
-        let filter = await Physicians./*.aggregate([
-            {
-                $match: {
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    specialty: req.body.specialty,
-                    contractStatus: req.body.contractStatus,
-                    term: {
-                        years: req.body.termYears,
-                        months: req.body.termMonths,
-                        start: req.body.termStart,
-                        end: req.body.termEnd
-                    },
-                    state: req.body.state,
-                    facility:[{
-                        name:req.body.facilityName,
-                        address: { street: req.body.facilityStreet, city: req.body.facilityCity, suite: req.body.facilitySuite},
-                        county: req.body.facilityCounty
-                    }]
-                }
-            }
-        ]);*/
-            find({
-                $or: [
-                    {firstName: req.body.firstName},
-                    {lastName: req.body.lastName},
-                    {"specialty.specialty": req.body.specialty},
-                    {"contractStatus.employeed": req.body.contractStatus},
-                    {"term.years": req.body.termYears},
-                    {"term.months": req.body.termMonths},
-                    {"term.start": req.body.termStart},
-                    {"term.end": req.body.termEnd},
-                    {state: req.body.state},
-                    {"facility.name": req.body.facilityName},
-                    {"facility.address.street": req.body.facilityStreet},
-                    {"facility.address.city": req.body.facilityCity},
-                    {"facility.address.suite": req.body.facilitySuite},
-                    {"facility.county": req.body.facilityCounty}
-                ]
-            },null,{limit: 50})
+        let filter = await Physicians.find({
+            $or: [
+                { firstName: req.body.firstName },
+                { lastName: req.body.lastName },
+                { "specialty.specialty": req.body.specialty },
+                { "contractStatus.employeed": req.body.contractStatus },
+                { "term.years": req.body.termYears },
+                { "term.months": req.body.termMonths },
+                { "term.start": req.body.termStart },
+                { "term.end": req.body.termEnd },
+                { state: req.body.state },
+                { "facility.name": req.body.facilityName },
+                { "facility.address.street": req.body.facilityStreet },
+                { "facility.address.city": req.body.facilityCity },
+                { "facility.address.suite": req.body.facilitySuite },
+                { "facility.county": req.body.facilityCounty }
+            ]
+        }, null, { limit: 50 })
         return res.send(filter);
 
     } catch (error) {
