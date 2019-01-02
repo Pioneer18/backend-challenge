d921cbea81d18639a90c4d95c919c9f252f9e7e8
sellsj14 <sellsj14@gmail.com>
2019-01-02 11:37:01 -0500

    adding a custom search route to find all physicians matching one or more of listed conditions and rerturning only their selected path values; e.g. physicians in Hillsborough  and/or in AdventHealth - return their name and specialty

---
+++ src/controllers/physicians/custom.js
@@ -0,0 +1,3 @@
+//find physicians by not-strict filter and return selected path values
+//e.g. find all physicians in Tampa General or specialty x-rays or term.years > 3 years, and select by name
+// returns: all physicians with one or more of the listed conditions will have their name returned
