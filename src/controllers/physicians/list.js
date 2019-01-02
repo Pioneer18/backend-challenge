90ec09394e4d9a274e3b1adf34ca9173c8cd4664
sellsj14 <sellsj14@gmail.com>
2019-01-01 19:21:59 -0500

    .find() worked with original promise structure, retrying with async again

--- src/controllers/physicians/list.js
+++ src/controllers/physicians/list.js
@@ -1,5 +1,5 @@
 //retrieve and list all the physicians from the db
-
+/*
 const list = ({ Physicians }) => async (req, res, next) => {
     try {
         await Physicians.find({});
@@ -10,5 +10,17 @@ const list = ({ Physicians }) => async (req, res, next) => {
         next(error);
     }
 };
+*/
+const list = ({ Physicians }) => (req, res, next) => {
+    Physicians.find()
+     .then( physicians => {
+         res.send(physicians);
+     })
+     .catch(err => {
+         res.send({
+             message: err.message || "error error error"
+         })
+     })
+}
 
 module.exports = { list };
