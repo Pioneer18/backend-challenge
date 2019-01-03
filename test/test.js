cfd58616b0b33e69b45c102657fad5bc1d8a56bb
sellsj14 <sellsj14@gmail.com>
2019-01-02 21:50:31 -0500

    npm "test" script worked, and test results indicate that there is not validation on my Physician model's paths yet; I know this is true and the test worked. will add required to the paths and run test with expect().to.not.exist;

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@
 //import chai for easy assertions
 const expect = require('chai').expect;
 //import the physician model to validate
-const { Physician } = require('../src/models/physicians');
+const  { Physicians }  = require('../src/models/physicians');
 
 //run test with Mocha and chai
 describe('Physcian Model Validation', (done) => {
-    const p = new { Physician };
+    const p = new Physicians({});
 
-    p.validate((err) => {
+    p.validate( err => {
         expect(err.errors.Physicians).to.exist; //Physician.save() with no required paths
         done();
     })
