bf977619fdb7be89994df4bb018651d6651cd105
sellsj14 <sellsj14@gmail.com>
2019-01-02 21:33:26 -0500

    preparing to test the Physcian models validation, that is ensure that the paths are all required so no empty physicians are entered into the db

---
+++ test/test.js
@@ -0,0 +1,16 @@
+//Test that the mongoose Physician Model cannot be created with any empty Schema Path values
+
+//import chai for easy assertions
+const  expect = require('chai').expect;
+//import the physician model to validate
+const Physician = require('../src/models/physicians');
+
+//run test with Mocha and chai
+describe('Physcian Model Validation',(done)=>{
+    const p = new Physician();
+
+    p.validate( (err) => {
+        expect(err.errors.Physicians).to.exist; //Physician.save() with no required paths
+        done();
+    })
+})
